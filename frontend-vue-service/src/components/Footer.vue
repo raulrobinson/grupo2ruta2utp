c419ee32f486273875ce3d88a5b06bd622e1fbb5
rasysbox <71100949+raulrobinson@users.noreply.github.com>
2022-10-03 20:57:22 -0500

    Frontend Vue

---
+++ frontend-vue-service/src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="footer">
+        <footer>
+            Raul Bolivar
+        </footer>
+    </div>
+</template>
+<script>
+export default {
+    name: 'FooterView',
+    components: {
+    }
+}
+</script>
+<style  scoped>
+footer {
+    position: fixed;
+    height: 100px;
+    bottom: 0;
+    width: 100%;
+    background-color: black;
+    color: blanchedalmond;
+}
+</style>
--- ms-product-service/src/main/java/com/grupo2/utp/ProductMainApplication.java
+++ ms-product-service/src/main/java/com/grupo2/utp/ProductMainApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppApplication {
+public class ProductMainApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AppApplication.class, args);
+        SpringApplication.run(ProductMainApplication.class, args);
     }
 }
